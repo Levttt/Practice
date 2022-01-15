50eedf0e0ddd4e2938c211bb5e39a64b7fe067ac
Levttt <leo518000@gmail.com>
2022-01-15 15:29:31 +0200

    Exercise 2 remade

--- task_2.php
+++ task_2.php
@@ -1,19 +1,16 @@
 <?php
-$privacyInfo = '<h2>Privacy</h2>';
-$privacyInfoDescription = '<p class="mb-g">
-                                Your privacy is important to us at SmartAdmin and the protection, confidentiality and integrity of your personal data are our prime concerns.
-                                We will only use your personal information to administer your account, provide the products and services you have requested from us, and to keep you informed about our products and services (if you have consented to this).
-                                We only use your data for the purposes for which it was collected and, where relevant, to meet local legal obligations.
-                                We will retain your personal information only for as long as is necessary for the purposes for which the information was collected, or as long as is required pursuant to law.
-                            </p>';
-$coociesInfo = '<h3>Cookies and other similar technologies</h3>';
-$coociesInfoDescription = 'p>
-                                We collect certain data through cookies and similar technologies (e.g. web beacons, tags, device identifiers). Cookies are text files placed on your computer to collect standard internet log information and visitor behaviour information. This information is used to track visitor use of the website and to compile statistical reports on website activity. We register your interaction with our services in order to improve our website, content and services. Our use of such technologies and the data collected is described in more detail in our Cookie Policy. You can manage your cookie preferences through your browser settings.
-                            </p>';
-$info = array($privacyInfo, $privacyInfoDescription, $coociesInfo, $coociesInfoDescription);
+$db = [
+        [
+                'header1' => 'Privacy', 'info1' => 'Your privacy is important to us at SmartAdmin and the protection, confidentiality and integrity of your personal data are our prime concerns.
+                            We will only use your personal information to administer your account, provide the products and services you have requested from us, and to keep you informed about our products and services (if you have consented to this).
+                            We only use your data for the purposes for which it was collected and, where relevant, to meet local legal obligations.
+                            We will retain your personal information only for as long as is necessary for the purposes for which the information was collected, or as long as is required pursuant to law.'
+        ],
+        [
+                'header2' => 'Cookies and other similar technologies', 'info2' => 'We collect certain data through cookies and similar technologies (e.g. web beacons, tags, device identifiers). Cookies are text files placed on your computer to collect standard internet log information and visitor behaviour information. This information is used to track visitor use of the website and to compile statistical reports on website activity. We register your interaction with our services in order to improve our website, content and services. Our use of such technologies and the data collected is described in more detail in our Cookie Policy. You can manage your cookie preferences through your browser settings.'
+        ]
+];
 ?>
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -50,13 +47,16 @@ $info = array($privacyInfo, $privacyInfoDescription, $coociesInfo, $coociesInfoD
             <div class="panel-container show">
                 <div class="panel-content">
                     <div class="fs-lg fw-300 p-5 bg-white border-faded rounded mb-g">
-
-                        <?php
-                        foreach ($info as $val){
-                            echo $val;
-                        }
-                        ?>
-
+                        <?php foreach ($db as $val): ?>
+                        <h2><?php echo $val['header1']; ?></h2>
+                        <p class="mb-g">
+                            <?php echo $val['info1']; ?>
+                        </p>
+                        <h3> <?php echo $val['header2']; ?> </h3>
+                        <p>
+                            <?php echo $val['info2']; ?>
+                        </p>
+                        <?php endforeach; ?>
                     </div>
                 </div>
             </div>
