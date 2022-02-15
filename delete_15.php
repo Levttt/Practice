35e9b73c25f375dc923c79d449c83585e88cc65f
Levttt <leo518000@gmail.com>
2022-02-15 13:05:12 +0200

    Exercise 15_1

--- delete_15.php
+++ delete_15.php
@@ -11,10 +11,9 @@ $record = $query->fetch();
 $imgUrl = __DIR__ . 'img/demo/gallery/' . $record['img_name'];
 if(file_exists($imgUrl)){
     unlink($imgUrl);
+
+    $sql = 'DELETE FROM users.images WHERE id = (:id)';
+    $query = $pdo->prepare($sql);
+    $query->execute(['id'=>$id]);
 }
-
-$sql = 'DELETE FROM users.images WHERE id = (:id)';
-$query = $pdo->prepare($sql);
-$query->execute(['id'=>$id]);
-
 header('Location: /task_15_1.php');
